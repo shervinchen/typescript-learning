950c8cc9ca26fc43cfa56aa51d4f430ce66f4234
csdoker <csd758371536@qq.com>
2021-03-21 23:47:17 +0800

    feat: add dragable-div project

---
+++ dragable-div/main.ts
@@ -0,0 +1,36 @@
+// npx parcel index.html
+
+// var a: string = 'hi'
+// console.log('hi')
+
+const div = document.createElement('div')
+div.style.border = '1px solid red'
+div.style.height = '100px'
+div.style.width = '100px'
+div.id = 'demo'
+
+document.body.appendChild(div)
+
+let flag = false
+let position = [0, 0]
+
+div.onmousedown = e => {
+  flag = true
+  position = [e.clientX, e.clientY]
+}
+
+document.onmouseup = e => {
+  flag = false
+}
+
+document.onmousemove = e => {
+  if (flag) {
+    const deltaX = e.clientX - position[0]
+    const deltaY = e.clientY - position[1]
+    const top = parseInt(div.style.top!) || 0
+    const left = parseInt(div.style.left!) || 0
+    div.style.top = `${top + deltaY}px`
+    div.style.left = `${left + deltaX}px`
+    position = [e.clientX, e.clientY]
+  }
+}
