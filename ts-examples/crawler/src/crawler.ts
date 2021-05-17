d0a8092778f52323fa1a836ca2e6beafece3a2c2
csdoker <csdokerchen@yunify.com>
2021-05-17 13:53:24 +0800

    feat: add Analyzer class

--- ts-examples/crawler/src/crawler.ts
+++ ts-examples/crawler/src/crawler.ts
@@ -1,36 +1,24 @@
 import superagent from 'superagent'
-import cheerio from 'cheerio'
 import fs from 'fs'
 import path from 'path'
 
-interface Course {
-  title: string
-  count: number
-}
+import DellAnalyzer from './dellAnalyzer'
 
-interface CourseResult {
-  time: number
-  data: Course[]
-}
-
-interface JsonContent {
-  [propName: number]: Course[]
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
 }
 
 class Crawler {
-  private secret = 'x3b174jsx'
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
   private filePath = path.resolve(__dirname, '../data/course.json')
 
-  constructor() {
+  constructor(private url: string, private analyzer: Analyzer) {
     this.initSpiderProcess()
   }
 
   async initSpiderProcess() {
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    this.writeFile(JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyze(html, this.filePath)
+    this.writeFile(fileContent)
   }
 
   async getRawHtml() {
@@ -38,34 +26,13 @@ class Crawler {
     return result.text
   }
 
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    const courseInfos: Course[] = []
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
-      courseInfos.push({ title, count })
-    })
-    return {
-      time: (new Date()).getTime(),
-      data: courseInfos
-    }
-  }
-
-  generateJsonContent(courseInfo: CourseResult) {
-    let fileContent: JsonContent = {}
-    if (fs.existsSync(this.filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
-  }
-
   writeFile(content: string) {
     fs.writeFileSync(this.filePath, content)
   }
 }
 
-const crawler = new Crawler()
+const secret = 'x3b174jsx'
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+
+const analyzer = new DellAnalyzer()
+new Crawler(url, analyzer)
