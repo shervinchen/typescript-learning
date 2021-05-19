2d9fd9944dd88c4966ed484430687814f3c53cd2
csdoker <csdokerchen@yunify.com>
2021-05-19 09:53:04 +0800

    feat: finish 4-16

--- ts-examples/4-11/src/page.ts
+++ ts-examples/4-11/src/page.ts
@@ -1,8 +1,32 @@
-import $ from 'jquery'
+// import $ from 'jquery'
+
+// {
+//   $(function () {
+//     $('body').html('<div>123</div>')
+//     new $.fn.init()
+//   })
+// }
 
 {
-  $(function () {
-    $('body').html('<div>123</div>')
-    new $.fn.init()
+  interface Person {
+    name: string
+    age: number
+    gender: string
+  }
+  class Teacher {
+    constructor(private info: Person) { }
+
+    getInfo<T extends keyof Person>(key: T): Person[T] {
+      return this.info[key]
+    }
+  }
+
+  const teacher = new Teacher({
+    name: 'csd',
+    age: 18,
+    gender: 'male'
   })
+
+  const test = teacher.getInfo('name')
+  console.log(test)
 }
