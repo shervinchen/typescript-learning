2d2b945aff9a22687b1bf599f9837c0be7638c7b
csdoker <csdokerchen@yunify.com>
2021-05-25 14:05:56 +0800

    feat: add router

--- ts-examples/react-crawler/src/App.tsx
+++ ts-examples/react-crawler/src/App.tsx
@@ -1,49 +1,16 @@
-import { Form, Input, Button } from 'antd';
-import { LockOutlined } from '@ant-design/icons';
-import './login.css';
+import { Route, Switch, HashRouter } from 'react-router-dom';
+import Login from './pages/Login';
 
-const NormalLoginForm = () => {
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-  };
-
+const App = () => {
   return (
-    <div className="login-container">
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Password!',
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 };
 
-export default NormalLoginForm;
+export default App;
