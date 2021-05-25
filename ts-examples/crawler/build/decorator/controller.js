f84d1fb0511ab4f87d05b4336a72845ac3225cca
csdoker <csdokerchen@yunify.com>
2021-05-25 10:28:10 +0800

    feat: init react-crawler project

---
+++ ts-examples/crawler/build/decorator/controller.js
@@ -0,0 +1,32 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.controller = void 0;
+var router_1 = __importDefault(require("../router"));
+function controller(root) {
+    return function (target) {
+        for (var key in target.prototype) {
+            var path = Reflect.getMetadata('path', target.prototype, key);
+            var method = Reflect.getMetadata('method', target.prototype, key);
+            var middlewares = Reflect.getMetadata('middlewares', target.prototype, key);
+            var handler = target.prototype[key];
+            if (path && method) {
+                var fullPath = root === '/' ? path : "" + root + path;
+                if (middlewares && middlewares.length) {
+                    router_1.default[method].apply(router_1.default, __spreadArray(__spreadArray([fullPath], middlewares), [handler]));
+                }
+                else {
+                    router_1.default[method](fullPath, handler);
+                }
+            }
+        }
+    };
+}
+exports.controller = controller;
---
+++ ts-examples/crawler/build/decorator/use.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.use = void 0;
+function use(middleware) {
+    return function (target, key) {
+        var originMiddlewares = Reflect.getMetadata('middlewares', target, key) || [];
+        originMiddlewares.push(middleware);
+        Reflect.defineMetadata('middlewares', originMiddlewares, target, key);
+    };
+}
+exports.use = use;
