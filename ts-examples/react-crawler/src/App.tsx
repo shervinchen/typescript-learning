1e55ae6ac347053fdbe2bf73d1141469d428fe4e
csdoker <csdokerchen@yunify.com>
2021-05-25 11:49:23 +0800

    feat: add form component

--- ts-examples/react-crawler/src/App.tsx
+++ ts-examples/react-crawler/src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { Form, Input, Button } from 'antd';
+import { LockOutlined } from '@ant-design/icons';
+import './login.css';
 
-function App() {
+const NormalLoginForm = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div className="App">
-      hello world
+    <div className="login-container">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
-export default App;
+export default NormalLoginForm;
