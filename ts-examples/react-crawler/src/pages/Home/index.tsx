f308d21d3a25783b23b3d2773bb1c878c96e355d
csdoker <csdokerchen@yunify.com>
2021-05-28 16:42:04 +0800

    feat: add data of echarts

--- ts-examples/react-crawler/src/pages/Home/index.tsx
+++ ts-examples/react-crawler/src/pages/Home/index.tsx
@@ -3,15 +3,37 @@ import { Redirect } from 'react-router-dom';
 import { Button, message } from 'antd';
 import axios from 'axios';
 import ReactEcharts from 'echarts-for-react';
+import moment from 'moment';
 import './index.css';
+
+interface CourseItem {
+  title: string;
+  count: number;
+}
+
+interface Data {
+  [key: string]: CourseItem[];
+}
+
+interface LineData {
+  name: string;
+  type: string;
+  data: number[];
+}
 
 const Home = () => {
   const [loginStatus, setLoginStatus] = useState(true);
+  const [data, setData] = useState<Data>({});
 
   useEffect(() => {
     axios.get('/api/isLogin').then((res) => {
       if (!res.data?.data) {
         setLoginStatus(false);
+      }
+    });
+    axios.get('/api/showData').then((res) => {
+      if (res.data?.data) {
+        setData(res.data.data);
       }
     });
   }, []);
@@ -35,7 +57,58 @@ const Home = () => {
   };
 
   const getOption: () => echarts.EChartOption = () => {
-    return {};
+    const courseNames: string[] = [];
+    const times: string[] = [];
+    const tempData: {
+      [key: string]: number[];
+    } = {};
+    for (let i in data) {
+      const item = data[i];
+      times.push(moment(Number(i)).format('MM-DD HH:mm'));
+      item.forEach((innerItem) => {
+        const { title, count } = innerItem;
+        if (courseNames.indexOf(title) === -1) {
+          courseNames.push(title);
+        }
+        tempData[title]
+          ? tempData[title].push(count)
+          : (tempData[title] = [count]);
+      });
+    }
+    const result: LineData[] = [];
+    for (let i in tempData) {
+      result.push({
+        name: i,
+        type: 'line',
+        data: tempData[i],
+      });
+    }
+    return {
+      title: {
+        text: '课程在线学习人数',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: courseNames,
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: times,
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: result,
+    };
   };
 
   return (
